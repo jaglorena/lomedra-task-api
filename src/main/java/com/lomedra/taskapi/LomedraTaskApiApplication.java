916f3f8f741e2f308639269b6ff7f024d739b0ed
jaglorena <loremedi0103@gmail.com>
2026-08-28 23:11:48 -0600

    chore: initialize Spring Boot API project

---
+++ src/main/java/com/lomedra/taskapi/LomedraTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.lomedra.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomedraTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LomedraTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lomedra/taskapi/LomedraTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lomedra.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LomedraTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
